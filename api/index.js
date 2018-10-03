38d4a1e8c33cebeb28395db8dd5af7bd180442f1
prayashm97 <mprayash@gmail.com>
2018-10-03 09:16:04 -0400

    dev branch init

---
+++ api/index.js
@@ -0,0 +1,8 @@
+/* tslint:disable:no-console */
+
+import app from "./app";
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("Express server listening on port " + PORT);
+});

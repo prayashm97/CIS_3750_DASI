167742b94f18fb4e87a1cf883abf34e394aac367
Ian Cross <icross@mail.uoguelph.ca>
2018-11-14 11:18:02 -0500

    Creating login and signup pages

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { Login } from "../src/components/login";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: "no",
+    }
+  }
+
+  handleLogin = (email, password) => {
+    this.setState({login: "yes"});
+    console.log(email, password);
+  }
+
   render() {
-    return (
-      <div className="App">
-        This is our app
+    const { login } = this.state;
+
+    let renderContent;
+
+    if (login === "no") {
+      renderContent = <Login handleLogin={this.handleLogin}/>
+    } else if (login === "yes") {
+      renderContent = <React.Fragment>
+        Congradulations You logged In
+      </React.Fragment>
+    } else {
+      renderContent = <div>
+        Something went wrong, you shouldn't see this.
       </div>
-    );
+    }
+
+    return <React.Fragment>{renderContent}</React.Fragment>
   }
 }
 
